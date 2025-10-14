ffdb320a7f1b839cf092802e6a6dda0ae3bff821
fallensakura <fallensakura715@gmail.com>
2025-10-14 20:40:18 +0800

    1

---
+++ P01-server/src/main/java/com/fallensakura/p01server/P01ServerApplication.java
@@ -0,0 +1,13 @@
+package com.fallensakura.p01server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P01ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P01ServerApplication.class, args);
+    }
+
+}
