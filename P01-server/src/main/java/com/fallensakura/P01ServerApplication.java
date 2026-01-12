4f9d9b4fecfb8676305fe16524218642e8050d93
fallensakura <fallensakura715@gmail.com>
2026-01-12 11:20:59 +0800

    feat: Add Spring Task

--- P01-server/src/main/java/com/fallensakura/P01ServerApplication.java
+++ P01-server/src/main/java/com/fallensakura/P01ServerApplication.java
@@ -2,8 +2,14 @@ package com.fallensakura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableCaching
+@EnableTransactionManagement
 public class P01ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(P01ServerApplication.class, args);
