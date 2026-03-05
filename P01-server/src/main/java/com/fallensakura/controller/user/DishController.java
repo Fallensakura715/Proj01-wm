d5fcdcd554f530e170197592d7e843fcf8ca5d7b
fallensakura <fallensakura715@gmail.com>
2026-03-05 13:53:01 +0800

    feat(client): add dish query features

---
+++ P01-server/src/main/java/com/fallensakura/controller/user/DishController.java
@@ -0,0 +1,29 @@
+package com.fallensakura.controller.user;
+
+import com.fallensakura.entity.Dish;
+import com.fallensakura.result.Result;
+import com.fallensakura.service.DishService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/dish")
+@RequiredArgsConstructor
+@Tag(name = "C端-菜品浏览接口")
+public class DishController {
+
+    private final DishService dishService;
+
+    @GetMapping("/list")
+    @Operation(summary = "根据分类id查询菜品")
+    public Result<List<Dish>> getDishedByCategoryId(@RequestParam Long id) {
+        return Result.success(dishService.selectByCategoryId(id));
+    }
+}
